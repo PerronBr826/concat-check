89738c0a195230937bb229513e688755b27434a3
PerronBr826 <PerronBr826@northwested.org>
2024-09-20 10:29:48 -0400

    Initial commit

---
+++ concat_mc.py
@@ -0,0 +1,16 @@
+# Bryce Perron
+# 9/20/2024
+
+# Task 1
+first_name = "Bryce"
+favorite_game = "Fallout: New Vegas"
+print(first_name.title() + "'s favorite Video Game is " + favorite_game.title())
+
+# Task 2
+print("What I've learned about concatenation:")
+print("- I have learned how to actually spell it")
+print("- I have learned how to use it to combine strings")
+
+# Task 3
+bk_price = 5.99
+print("At Burger King, one Whopper with cheese will cost you $" + str(bk_price) + ".")
